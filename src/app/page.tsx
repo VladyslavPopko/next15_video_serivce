298fa32fb1cd89bb8a6bfd6a6a33885cba446bca
Vladyslav Popko <32167033+VladyslavPopko@users.noreply.github.com>
2025-01-06 15:35:39 +0200

    change taliwindconfig, add colors, install sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
